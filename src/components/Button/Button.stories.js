f21b524c00a9b2704913da2b9bd9551ecb9dc539
Aryan Dutt <aryan.dutt@aryan.dutt-C02G89ZSML7H>
2023-06-04 16:52:29 +0530

    second comitt

--- src/components/Button/Button.stories.js
+++ src/components/Button/Button.stories.js
@@ -8,7 +8,7 @@ export default{
     component: Button
 }
 
-export const Primary = () => <Button variant = 'primary'>Primary</Button>
+export const Primary = () => <Button variant = 'primary'>Primary sprinkl</Button>
 export const Secondary = () => <Button variant = 'secondary'>Secondary</Button>
 export const Success = () => <Button variant = 'success'>Success</Button>
 export const Danger = () => <Button variant = 'danger'>Danger</Button>
